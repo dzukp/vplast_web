0a792cc05d10a87c25e8720285fbe198fca9af61
Zolotarev P <dzukp@list.ru>
2023-09-26 00:05:22 +0300

    3-я линия

--- app/service.py
+++ app/service.py
@@ -61,6 +61,7 @@ class MbMapItem:
     start_addr: int
     quantity: int
     data_format: str
+    func: int
 
 
 class DataHolder:
@@ -82,7 +83,7 @@ class DataHolder:
 
     def mb_init(self):
         self.logger.debug('mb_init')
-        self.mb_master = modbus_tcp.TcpMaster(host=self.host, port=self.port, timeout_in_sec=0.5)
+        self.mb_master = modbus_tcp.TcpMaster(host=self.host, port=self.port, timeout_in_sec=0.25)
 
     def map_init(self, mb_map):
         for m in mb_map:
@@ -96,7 +97,7 @@ class DataHolder:
             return
         for m in self.mb_map:
             try:
-                data = self.mb_master.execute(1, cst.READ_INPUT_REGISTERS, m.start_addr, m.quantity, m.data_format)
+                data = self.mb_master.execute(1, m.func, m.start_addr, m.quantity, m.data_format)
                 self.logger.debug(data)
             except socket.timeout:
                 self.logger.error('socket timeout')
@@ -129,20 +130,15 @@ def init_service():
     mb_map = [
         MbMapItem(
             items=[
-                MapItem(address=1, name='Счетчик', id='counter', format='>H')
-            ],
-            quantity=1,
-            start_addr=1,
-            data_format='>H'),
-        MbMapItem(
-            items=[
                 MapItem(address=154, name='Экструдер', id='extruder', format='>h', k=0.1),
                 MapItem(address=165, name='Протяжка', id='pulling', format='>h', k=0.1),
                 MapItem(address=175, name='Т1', id='t1', format='>h', k=0.1),
             ],
             quantity=175 - 154 + 2,
             start_addr=154,
-            data_format='>' + 'h' * (175 - 154 + 2)),
+            data_format='>' + 'h' * (175 - 154 + 2),
+            func=cst.READ_INPUT_REGISTERS
+        ),
         MbMapItem(
             items=[
                 MapItem(address=112, name='Т2', id='t2', format='>h', k=0.1),
@@ -159,7 +155,16 @@ def init_service():
             ],
             quantity=112 - 12 + 2,
             start_addr=12,
-            data_format='>' + 'h' * (112 - 12 + 2))
+            data_format='>' + 'h' * (112 - 12 + 2),
+            func=cst.READ_INPUT_REGISTERS),
+        MbMapItem(
+            items=[
+                MapItem(address=1, name='Счетчик', id='counter', format='>H')
+            ],
+            quantity=1,
+            start_addr=1,
+            data_format='>H',
+            func=cst.READ_INPUT_REGISTERS)
     ]
     dh = DataHolder(host=config.LINE1_HOST, mb_map=mb_map, logger=logging.getLogger(f'service.server_{server_name}'))
     data_holders[server_name] = dh
@@ -168,7 +173,38 @@ def init_service():
     dh = DataHolder(host=config.LINE2_HOST, mb_map=mb_map, logger=logging.getLogger(f'service.server_{server_name}'))
     data_holders[server_name] = dh
 
-    # server_name = '3'
-    # dh = DataHolder(host='192.168.0.16:5204', mb_map=mb_map, logger=logging.getLogger(f'service.server_{server_name}'))
-    # data_holders[server_name] = dh
+    server_name = '3'
+    mb_map = [
+        MbMapItem(
+            items=[
+                MapItem(address=1, name='Экструдер', id='extruder', format='>h'),
+                MapItem(address=2, name='Соэкструдер', id='coextruder', format='>h'),
+                MapItem(address=3, name='Протяжка', id='pulling', format='>h'),
+                MapItem(address=4, name='Т1', id='t1', format='>h'),
+                MapItem(address=5, name='Т2', id='t2', format='>h'),
+                MapItem(address=6, name='Т3', id='t3', format='>h'),
+                MapItem(address=7, name='Т4', id='t4', format='>h'),
+                MapItem(address=8, name='Т5', id='t5', format='>h'),
+                MapItem(address=9, name='Т6', id='t6', format='>h'),
+                MapItem(address=10, name='Т7', id='t7', format='>h'),
+                MapItem(address=11, name='Т8', id='t8', format='>h'),
+                MapItem(address=12, name='Т9', id='t9', format='>h'),
+                MapItem(address=13, name='Т10', id='t10', format='>h'),
+                MapItem(address=14, name='Т11', id='t11', format='>h'),
+                MapItem(address=15, name='Т12', id='t12', format='>h'),
+                MapItem(address=16, name='Т13', id='t13', format='>h'),
+                MapItem(address=17, name='Т14', id='t14', format='>h'),
+                MapItem(address=18, name='Т15', id='t15', format='>h'),
+                MapItem(address=19, name='Т16', id='t16', format='>h'),
+                MapItem(address=20, name='Т17', id='t17', format='>h'),
+                MapItem(address=21, name='Т18', id='t18', format='>h'),
+                MapItem(address=22, name='Т19', id='t19', format='>h')
+            ],
+            quantity=25,
+            start_addr=1,
+            data_format='>' + 'h' * 25,
+            func=cst.READ_HOLDING_REGISTERS)
+    ]
+    dh = DataHolder(host=config.LINE3_HOST, mb_map=mb_map, logger=logging.getLogger(f'service.server_{server_name}'))
+    data_holders[server_name] = dh
     return data_holders
