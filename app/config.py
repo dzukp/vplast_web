9a85a7f9505b7e535433ecc95f9d746fe8bab69f
Zolotarev P <dzukp@list.ru>
2023-09-27 22:28:28 +0300

    поправлены адреса

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 LINE1_HOST = os.environ.get("LINE1_HOST", '192.168.0.157:5204')
-LINE2_HOST = os.environ.get("LINE2_HOST", '192.168.0.158:5204')
+LINE2_HOST = os.environ.get("LINE2_HOST", '192.168.0.169:5204')
 LINE3_HOST = os.environ.get("LINE3_HOST", '192.168.1.57')
